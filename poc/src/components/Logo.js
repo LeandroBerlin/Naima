451da56abf15cf8fb6349241f4b99f78620d0f39
Bru <annabru.abh@gmail.com>
2019-04-30 14:12:17 +0200

    Deleted the React default logo.

--- poc/src/components/Logo.js
+++ poc/src/components/Logo.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import logo from "../styles/logo.svg";
+// import logo from "../styles/logo.svg";
 export default class Logo extends Component {
   render() {
     return (
       <React.Fragment>
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Naima PoC</h1>
       </React.Fragment>
     );
